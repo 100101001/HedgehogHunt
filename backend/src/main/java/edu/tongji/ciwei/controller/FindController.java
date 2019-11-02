e3691d54e54b0841aa358807205084a515bf842f
100101001 <1552729@tongji.edu.cn>
2019-11-02 19:03:23 +0800

    Controller for released Found
    1、setup the class and test

---
+++ backend/src/main/java/edu/tongji/ciwei/controller/FindController.java
@@ -0,0 +1,28 @@
+package edu.tongji.ciwei.controller;
+
+import edu.tongji.ciwei.pojo.dto.response.GoodsDetail;
+import edu.tongji.ciwei.pojo.dto.response.ReleaseDate;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 100101001
+ * @date 11/2/2019 3:46 PM
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("find")
+public class FindController {
+
+    @GetMapping("test")
+    @ApiOperation(value="获取寻物发布项")
+    public GoodsDetail getGoodsDetail(){
+        GoodsDetail goods = new GoodsDetail();
+        goods.setReleaserName("韦朝旭");
+        goods.setReleaseDate(ReleaseDate.builder().year(2019).month(5).day(26).build());
+        return goods;
+    }
+}
