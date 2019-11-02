46c8fac4a776c53e09a507e0c0a67d5ba1888f30
100101001 <1552729@tongji.edu.cn>
2019-11-02 19:00:53 +0800

    Integrate Swagger2 for API doc

--- backend/src/main/java/edu/tongji/ciwei/CiweiApplication.java
+++ backend/src/main/java/edu/tongji/ciwei/CiweiApplication.java
@@ -2,8 +2,10 @@ package edu.tongji.ciwei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CiweiApplication {
 
     public static void main(String[] args) {
