2a9aeb7e09b60339b0141338183de377985d7841
100101001 <1552729@tongji.edu.cn>
2019-11-03 14:24:13 +0800

    set index in goodsList for newly loaded data

--- backend/src/main/java/edu/tongji/ciwei/controller/FoundController.java
+++ backend/src/main/java/edu/tongji/ciwei/controller/FoundController.java
@@ -2,6 +2,7 @@ package edu.tongji.ciwei.controller;
 
 import edu.tongji.ciwei.pojo.dto.response.GoodsDetail;
 import edu.tongji.ciwei.pojo.dto.response.ReleaseDate;
+import edu.tongji.ciwei.util.GoodsListFunction;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,30 +25,32 @@ import java.util.List;
 public class FoundController {
 
     @GetMapping("test")
-    @ApiOperation(value="获取寻物发布项的详情信息")
-    public GoodsDetail getGoodsDetail(){
+    @ApiOperation(value = "获取寻物发布项的详情信息")
+    public GoodsDetail getGoodsDetail() {
         GoodsDetail goods = new GoodsDetail();
         goods.setReleaserName("韦朝旭");
         goods.setReleaseDate(ReleaseDate.builder().year(2019).month(5).day(26).build());
         return goods;
     }
 
-    @ApiOperation(value = "分页获取寻物发布页面的列表" )
+    @ApiOperation(value = "分页获取寻物发布页面的列表")
     @GetMapping("{pageIndex}")
-    public List<GoodsDetail> getFoundGoodsList(@PathVariable String pageIndex, @PathParam("pageCount")Integer pageCount){
-        System.out.println(pageIndex);
-        System.out.println(pageCount);
+    public List<GoodsDetail> getFoundGoodsList(@PathVariable Integer pageIndex, @PathParam("pageCount") Integer pageCount) throws CloneNotSupportedException {
+        Integer idxStart = pageIndex * pageCount;
         List<GoodsDetail> list = new ArrayList<>();
         GoodsDetail goods = new GoodsDetail();
         goods.setReleaserName("李依璇");
         goods.setAvatarUrl("/images/images/avatar/1.png");
         goods.setGoodsImageUrl("https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2546335362.jpg");
-        goods.setGoodsDescription("我就测试一下好不好用");
+        goods.setGoodsBriefIntroduction("拾物简介14个字..");
+        goods.setGoodsDescription("拾物描述詳情");
+        goods.setLocationDescription("杨浦区四平路1239号");
         goods.setGoodsOwner("李依璇");
         goods.setGoodsType("钱包");
         goods.setReleaseDate(ReleaseDate.builder().year(2019).month(5).day(26).build());
         list.add(goods);
-        list.add(goods);
+        list.add(goods.clone());
+        GoodsListFunction.forEach(list.iterator(), (index, item) -> item.setGoodsListIndex(index + idxStart));
         return list;
     }
 }
