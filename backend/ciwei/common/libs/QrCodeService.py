efbbc57adaaf7ee21dc0643a9d3e6dd0141d0f1b
100101001 <1552729@tongji.edu.cn>
2019-12-08 22:51:01 +0800

    backend-qrcode from wx

---
+++ backend/ciwei/common/libs/QrCodeService.py
@@ -0,0 +1,10 @@
+from common.models.ciwei import QrCode
+
+
+def thankQrcode(codeId, orderId):
+    """
+       fill in  order_id to a qrcode record with specific id
+       :return:
+       """
+    # add orderId to codeId
+    pass
