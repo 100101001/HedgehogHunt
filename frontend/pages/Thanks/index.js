30c3802f14e15d42f379caba9d0c3abcaaee3bad
100101001 <1552729@tongji.edu.cn>
2020-02-18 22:31:39 +0800

    前端-支付-感谢页面
    1、金额选择
    2、发出感谢时，金额为0仅后端创建Thank记录，金额不为0则使用微信支付让用户支付商户钱，（商户再隔日到账？？）

--- frontend/pages/Thanks/index.js
+++ frontend/pages/Thanks/index.js
@@ -4,41 +4,130 @@ Page({
   data: {
     isHide: false,
     thanks_text: "",
-    price: 0
+    items: [
+      { "name": 0, value: "0元", price: 0, checked: "true" },
+      { "name": 1, value: "0.5元", price: 0.5 },
+      { "name": 2, value: "1元", price: 1 },
+      { "name": 3, value: "自定义", price: "" }
+    ],
+    checkedRadio: 0,
+    price: "",
+    canSendThank: false
   },
-  onLoad: function(options) {
+  onLoad: function (options) {
     var data = JSON.parse(options.data)
     this.setData({
       data: data
     })
   },
-  goHome: function(e) {
+  goHome: function (e) {
     wx.navigateTo({
       url: "../Find/Find?business_type=1",
     })
   },
-  listenerTextInput: function(e) {
+  listenerTextInput: function (e) {
     this.setData({
       thanks_text: e.detail.value
     });
   },
-  moneyThanks(e) {
-    app.alert({
-      'content': '调起微信支付'
-    });
+  listenerMoneyInput: function (e) {
+    console.log(e)
+    var value = e.detail.value
+    value = value.replace(/[^\d)]/g, '')
+    console.log(value)
+    this.setData({
+      price: value
+    })
   },
-  subMit: function(e) {
+  radioChange: function (e) {
+    var checkedRadio = e.detail.value
+    var items = this.data.items
+    for (var i = 0; i < items.length; i++) {
+      items[i]['checked'] = i == checkedRadio
+    }
+    this.setData({
+      checkedRadio: checkedRadio,
+      items: items
+    })
+  },
+  sendThank: function (e) {
+
+    //取出答谢金额
+    var items = this.data.items
+    var checkedRadio = this.data.checkedRadio
+    var price = this.data.price
+    price = checkedRadio == 3 ? price : items[checkedRadio]['price']
+
+    if (price == 0) {
+      //纯文字感谢
+      this.createThank(price)
+    } else {
+      //红包加文字感谢
+      var that = this
+      //后端下支付单
+      wx.request({
+        'url': app.buildUrl('/order/place'),
+        'header': app.getRequestHeader(),
+        'method': 'POST',
+        'data': {
+          'price': price
+        },
+        'success': function (res) {
+          console.log(res)
+          if (res.code == 200) {
+            //前端微信支付
+            data = res.data
+            wx.requestPayment({
+              timeStamp: data['timeStamp'],
+              nonceStr: data['nonceStr'],
+              package: data['package'],
+              signType: data['signType'],
+              paySign: data['paySign'],
+              complete: function (res) {
+                //后端查询支付状态
+                wx.request({
+                  url: app.buildUrl('/order/query'),
+                  method: 'post',
+                  header: app.getRequestHeader(),
+                  success: function (res) {
+                    resp = res.data
+                    if (resp.code == 200) {
+                      that.createThank(price, resp.data.order_id)
+                    } else {
+                      app.alert({ 'content': resp.msg })
+                    }
+                  },
+                  fail: function (res) {
+                    app.alert({ 'content': res.data.msg })
+                  }
+                })
+              }
+            })
+          } else {
+            wx.showToast({ title: JSON.stringify(data['msg']), icon: 'none' })
+          }
+        },
+        'fail': function (res) {
+          console.log(res)
+        }
+      })
+    }
+  },
+  //支付单id默认为空
+  createThank: function (price, order_id="") {
     var thanks_text = this.data.thanks_text;
     var data = this.data.data;
-    data['price'] = 0;
+    data['price'] = price;
     data['thanks_text'] = thanks_text;
+    data['order_id'] = order_id
     console.log(data);
-    var that=this;
+    var that = this;
+    //后端创建感谢记录
     wx.request({
       url: app.buildUrl("/thanks/create"),
       header: app.getRequestHeader(),
-      data:data,
-      success: function(res) {
+      data: data,
+      success: function (res) {
         var resp = res.data;
         if (resp.code !== 200) {
           app.alert({
@@ -54,7 +143,7 @@ Page({
         });
         that.goHome();
       },
-      fail: function(res) {
+      fail: function (res) {
         wx.hideLoading();
         wx.showToast({
           title: '系统繁忙，反馈失败！',
