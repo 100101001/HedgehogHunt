e6296d210ca8a6949d06e05dfff24b33f976c09a
100101001 <1552729@tongji.edu.cn>
2020-02-21 17:44:32 +0800

    前端-周边购买-产品页
    1、从Homepage点击学校可进入周边浏览页，浏览商品，加入购物车，进行购物

--- frontend/pages/Homepage/homepage.js
+++ frontend/pages/Homepage/homepage.js
@@ -10,25 +10,32 @@ Page({
     var unis = [{
       "id": "同济大学",
       "url": "/images/unis/TJU.jpeg",
-    }, {
+      "option": "1"
+      },
+      {
         "id": "北京大学",
         "url": "/images/unis/PKU.jpeg",
+        "option": "2"
       },
       {
         "id": "北京师范",
         "url": "/images/unis/BNU.jpeg",
+        "option": "3"
       },
       {
         "id": "东南大学",
         "url": "/images/unis/SEU.jpg",
+        "option": "4"
       },
       {
         "id": "西安交大",
         "url": "/images/unis/XJTU.jpeg",
+        "option": "5"
       },
       {
         "id": "四川大学",
         "url": "/images/unis/SCU.jpeg",
+        "option": "6"
       }
     ]
     //设置底部导航栏
@@ -71,5 +78,15 @@ Page({
   //点击导航
   onNavigateTap: function (event) {
     navigate.onNavigateTap(event, this)
+  },
+  goBuy: function (e) {
+    console.log(e)
+    var id = e.currentTarget.dataset.id
+    var campus = this.data.unis[id].option
+    var name = this.data.unis[id].id
+    console.log(campus)
+    wx.navigateTo({
+      url: 'product/index?campus=' + campus+'&campus_name='+ encodeURIComponent(name),
+    })
   }
 })
---
+++ frontend/pages/template/navigate-bar1/navigate-bar1-template.js
@@ -0,0 +1,20 @@
+const util = require("../../../utils/util.js");
+const app=getApp();
+const onNavigateTap = function(event, object) {
+    var id = event.currentTarget.dataset.id * 1; //乘1强制转换成数字
+    //TODO: 希望登录后直接进入页面
+    if((id==1||id==2) && !app.loginTip()){
+        return;
+    }
+    var [isSelecteds, urls] = util.onNavigateTap1(id);
+    object.setData({
+      isSelecteds: isSelecteds
+    })
+    wx.redirectTo({
+      url: urls[id],
+    })
+}
+
+module.exports = {
+    onNavigateTap:onNavigateTap
+}
