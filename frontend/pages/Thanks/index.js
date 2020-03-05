05b5967dd8b04ae2ad1f737166481cf747ccffe7
100101001 <1552729@tongji.edu.cn>
2020-03-06 05:13:00 +0800

    前端-答谢支付-逻辑更改
    金额答谢分下述三种情况
    1、账户没钱
    2、账户有钱，钱够
    3、账户钱够
    其中账户钱不够需要微信支付，分情况
    1、微信支付取消了，用户选择取消答谢，或者使用已扣的余额答谢
    2、微信支付了，正常走
    纯答谢：
    1、纯文字
    金额答谢也必须有文字

--- frontend/pages/Thanks/index.js
+++ frontend/pages/Thanks/index.js
@@ -17,7 +17,8 @@ Page({
   onLoad: function (options) {
     var data = JSON.parse(options.data)
     this.setData({
-      data: data
+      data: data,
+      balance: app.globalData.memberInfo.balance
     })
   },
   listenerTextInput: function (e) {
@@ -28,8 +29,7 @@ Page({
   listenerMoneyInput: function (e) {
     console.log(e)
     var value = e.detail.value
-    value = value.replace(/[^\d)]/g, '')
-    console.log(value)
+    value = value.replace(/[^\d.)]/g, '')
     this.setData({
       price: value
     })
@@ -46,85 +46,166 @@ Page({
     })
   },
   sendThank: function (e) {
-
-    //取出答谢金额
+    if(this.data.thanks_text == ""){
+      app.alert({
+        'title':'温馨提示',
+        'content':'别忘了用文字传递感谢~'
+      })
+      return
+    }
+    //取出答谢金额，可能是自定义金额或者指定金额
+    var price = this.data.price
     var items = this.data.items
     var checkedRadio = this.data.checkedRadio
-    var price = this.data.price
     price = checkedRadio == 3 ? price : items[checkedRadio]['price']
-
-    if (price == 0) {
+    // 金额合法性检测
+    var reg = /^[0-9]+([.]{1}[0-9]+){0,1}$/
+    if (!reg.test(price)) {
+      app.alert({
+        'title': '温馨提示',
+        'content': '请输入合法的钱数'
+      })
+      return
+    }
+    //分纯答谢和金额答谢
+    //金额答谢分纯账户和混合付款
+    if (price == 0) { 
       //纯文字感谢
       this.createThank(price)
     } else {
       //红包加文字感谢
+      var balance = this.data.balance
+      //答谢金额保留两位小数
+      var pay = Math.round(price * 100.6) / 100
+      //微信支付金额保留两位小数
+      var wx_pay = Math.round((pay - balance)*100)/100
+      var content = ''
+      if (wx_pay <= 0) {
+        //纯余额
+        content = (pay == price ? '' : '由于微信手续费，') + '将从您的余额扣除' + pay + '元'
+      } else {
+        //余额加现金支付
+        content = (pay == price ? '' : '由于微信手续费，') + 
+                  (balance==0? '':'从您账户扣除余额后，')+'您需支付' + wx_pay + '元'
+      }
       var that = this
-      //后端下支付单
-      wx.request({
-        'url': app.buildUrl('/thank/order/place'),
-        'header': app.getRequestHeader(),
-        'method': 'POST',
-        'data': {
-          'price': price
+      app.alert({
+        'title': '温馨提示',
+        'content': content,
+        'showCancel': true,
+        'cb_cancel': function(){
+
         },
-        'success': function (res) {
-          console.log(res)
-          var resp = res.data
-          var data = resp.data
-          if (resp.code == 200) {
-            //前端微信支付
-            wx.requestPayment({
-              timeStamp: data['timeStamp'],
-              nonceStr: data['nonceStr'],
-              package: data['package'],
-              signType: data['signType'],
-              paySign: data['paySign'],
-              complete: function (res) {
-                console.log(res)
-                if(res.errMsg == "requestPayment:fail cancel"){
-                  app.alert({'content': "支付失败"})
-                }
-                if(res.errMsg == "requestPayment:ok"){
-                  that.createThank(price, data.order_id)
-                }
-                //后端查询支付状态
-                // wx.request({
-                //   url: app.buildUrl('/thank/order/query'),
-                //   method: 'post',
-                //   header: app.getRequestHeader(),
-                //   success: function (res) {
-                //     resp = res.data
-                //     if (resp.code == 200) {
-                //       that.createThank(price, resp.data.order_id)
-                //     } else {
-                //       app.alert({ 'content': resp.msg })
-                //     }
-                //   },
-                //   fail: function (res) {
-                //     app.alert({ 'content': res.data.msg })
-                //   }
-                // })
+        'cb_confirm': function () {
+          //后端下支付单
+          wx.request({
+            'url': app.buildUrl('/thank/order/place'),
+            'header': app.getRequestHeader(),
+            'method': 'POST',
+            'data': {
+              'wx_price': wx_pay <= 0 ? 0 : wx_pay,
+              'account_price': wx_pay <= 0 ? pay : balance,
+              'pay_type': wx_pay <= 0 ? 'pure_balance' : 'mixed'
+            },
+            'success': function (res) {
+              console.log(res)
+              var resp = res.data
+              if (resp.code !== 200) {
+                app.alert({
+                  'content': resp.msg
+                })
+                return
               }
-            })
-          } else {
-            wx.showToast({ title: JSON.stringify(data['msg']), icon: 'none' })
-          }
-        },
-        'fail': function (res) {
-          console.log(res)
+              //更新账户余额
+              var data = resp.data
+              app.globalData.memberInfo.balance = data.balance
+              //纯账户扣款直接转入对方余额
+              if (resp.data.pay_type == "pure_balance") {
+                that.createThank(pay)
+                return
+              }
+              //发起微信支付
+              wx.requestPayment({
+                timeStamp: data['timeStamp'],
+                nonceStr: data['nonceStr'],
+                package: data['package'],
+                signType: data['signType'],
+                paySign: data['paySign'],
+                complete: function (res) {
+                  console.log(res)
+                  //取消支付
+                  if (res.errMsg == "requestPayment:fail cancel") {
+                    //用户取消了支付，且没有余额，不询问
+                    if (balance <= 0) {
+                      wx.showToast({
+                        title: '答谢失败',
+                        duration: 1200,
+                      })
+                      return
+                    }
+                    //用户取消了支付，询问余额是退还还是只用余额答谢
+                    app.alert({
+                      'title': '是否终止答谢',
+                      'content': "取消将仅用账户余额答谢",
+                      'showCancel': true,
+                      'cb_confirm': function () {
+                        //支付是因为余额全扣了也不够
+                        that.setBalance(balance)
+                      },
+                      'cb_cancel': function () {
+                        //支付是因为余额全扣了也不够，所以余额全给别人，等于纯余额答谢
+                        that.createThank(balance)
+                      }
+                    })
+                  }
+                  //支付成功
+                  if (res.errMsg == "requestPayment:ok") {
+                    that.createThank(pay, data.order_sn)
+                    return
+                  }
+                }
+              })
+            },
+            'fail': function (res) {
+              //无响应
+              app.serverBusy()
+            }
+          })
         }
       })
     }
   },
+  setBalance: function (amount = 0) {
+    var that = this
+    wx.request({
+      url: app.buildUrl('/thank/balance/rollback'),
+      method: 'post',
+      header: app.getRequestHeader(),
+      data: {
+        amount: amount
+      },
+      success: function (res) {
+        var resp = res.data
+        if (resp.code !== 200) {
+          app.alert({ 'content': '请联系开发者索要扣除的余额' })
+          return
+        }
+        var data = resp.data
+        app.globalData.memberInfo.balance = data.balance
+        that.goHome()
+      }
+    })
+  },
   //支付单id默认为空
-  createThank: function (price, order_id = "") {
+  createThank: function (price, order_sn = 'no') {
     var thanks_text = this.data.thanks_text;
     var data = this.data.data;
     data['target_price'] = price;
     data['thanks_text'] = thanks_text;
-    data['order_id'] = order_id
+    data['order_sn'] = order_sn
+    //答谢用户的名字
     data['owner_name'] = app.globalData.memberInfo.nickname
-    var that = this;
+    var that = this
     //后端创建感谢记录
     wx.request({
       url: app.buildUrl("/thanks/create"),
@@ -143,7 +224,7 @@ Page({
           title: '答谢成功！',
           icon: 'success',
           duration: 2000,
-          complete:function(){
+          complete: function () {
             that.goHome()
           }
         })
