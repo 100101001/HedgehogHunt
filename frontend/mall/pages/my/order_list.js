c720c1feaec7991ef72ab115d92f9ae6d6370e94
100101001 <1552729@tongji.edu.cn>
2020-03-28 20:59:44 +0800

    前端SX000007
    1、使用前端控制，支付订单后扣除用户余额
    2、使用前端判断支付的订单中是否包含非周边商品，充值短信和闪寻码，并进行相应的后端请求数据操作
    3、获取了闪寻码或短信充值后判断如果不成功就去联系技术人员操作。
    1、提供用户充值短信的接口
    2、提供闪寻码使用简介

--- frontend/mall/pages/my/order_list.js
+++ frontend/mall/pages/my/order_list.js
@@ -1,15 +1,185 @@
-var app = getApp();
+const app = getApp();
 
-const getQrcodeFromWechat = function() {
+/**
+ * getQrcodeFromWechat
+ * 获取二维码
+ */
+const getQrcodeFromWechat = function(cb_success=()=>{}) {
     wx.request({
         method: 'post',
         url: app.buildUrl('/qrcode/wx'),
         header: app.getRequestHeader(),
         success: function (res) {
+            let resp = res.data
+            if(resp['code']!==200){
+                return
+            }
             app.globalData.has_qrcode = true
+            cb_success()
         },
         fail: function (res) {
             app.serverBusy()
+        }
+    })
+}
+
+
+const smsBuySuccessCommonCallback = function (msg) {
+    wx.showToast({
+        title: msg,
+        duration: 600,
+        success:()=>{
+            setTimeout(()=>{
+                wx.navigateBack()
+            }, 600)
+        }
+    })
+}
+
+/**
+ * smsBuyFailCommonCallback
+ * 购买短信套餐包和短信服务并支付后，如果操作失败通用的操作提示
+ */
+const smsBuyFailCommonCallback = function () {
+    app.serverBusy(()=>{
+        app.alert({
+            title: "跳转提示",
+            content: '系统生病了，联系技术人员帮您购买',
+            cb_confirm: ()=>{
+                wx.navigateTo({
+                    url: '/pages/Mine/connect/index'
+                })
+            }
+        })
+    })
+}
+
+/**
+ * 为用户增加一个短信套餐包
+ * @param buy_times
+ * @param cb_success
+ */
+const addSmsPkg = function (cb_success=()=>{}) {
+    wx.request({
+        url: app.buildUrl('/member/sms/pkg/add'),
+        header: app.getRequestHeader(),
+        success: res => {
+            let resp = res.data
+            if (resp['code'] !== 200) {
+                smsBuyFailCommonCallback()
+                return
+            }
+            cb_success()
+        },
+        fail: res => {
+            smsBuyFailCommonCallback()
+        }
+    })
+}
+
+/**
+ * changeMemberSmsTimes
+ * 会员增加通知次数（改变）
+ * @param buy_times
+ * @param cb_success
+ */
+const changeMemberSmsTimes = function (buy_times, cb_success=()=>{}) {
+    wx.request({
+        url: app.buildUrl('/member/sms/change'),
+        header: app.getRequestHeader(),
+        data: {
+            times: buy_times
+        },
+        success: res => {
+            let resp = res.data
+            if (resp['code'] !== 200) {
+                smsBuyFailCommonCallback()
+                return
+            }
+            cb_success()
+        },
+        fail: res => {
+            smsBuyFailCommonCallback()
+        }
+    })
+}
+
+/**
+ * hasQrcode 设置用户是否有二维码
+ * @param cb_success
+ */
+const hasQrcode = function (cb_success=()=>{}) {
+    wx.request({
+        url: app.buildUrl("/member/has-qrcode"),
+        header: app.getRequestHeader(),
+        success: res => {
+            let resp = res.data
+            if (resp['code'] !== 200) {
+                return
+            }
+            cb_success(resp['data']['has_qr_code'])
+        }
+    })
+}
+
+/**
+ * orderPay 订单缴费
+ * @param order_sn 订单流水号
+ * @param cb_success 回调函数
+ */
+const orderPay = function (order_sn, cb_success=()=>{}) {
+    wx.request({
+        url: app.buildUrl("/order/pay"),
+        header: app.getRequestHeader(),
+        method: 'POST',
+        data: {
+            order_sn: order_sn
+        },
+        success: function (res) {
+            let resp = res.data;
+            if (resp['code'] != 200) {
+                app.alert({content: resp['msg']});
+                return
+            }
+            let pay_info = resp['data']['pay_info'];
+            wx.requestPayment({
+                timeStamp: pay_info.timeStamp,
+                nonceStr: pay_info.nonceStr,
+                package: pay_info.package,
+                signType: 'MD5',
+                paySign: pay_info.paySign,
+                success: function (res) {
+                    //支付成功
+                    cb_success()
+                },
+                fail: function (res) {
+                }
+            })
+        }
+    })
+}
+
+/**
+ * 用户余额扣除
+ * @param unit 余额变化
+ * @param cb_success 回调函数
+ */
+const changeUserBalance = function (unit = 0, cb_success = () => {}) {
+    wx.showLoading({
+        title: "扣除余额中"
+    })
+    wx.request({
+        url: app.buildUrl("/member/balance/change"),
+        header: app.getRequestHeader(),
+        data: {
+            unit: unit,
+            note: "在线购物"
+        },
+        success: res => {
+            cb_success()
+        },
+        complete: res => {
+            wx.hideLoading()
         }
     })
 }
@@ -29,9 +199,8 @@ Page({
      * @param e
      */
     statusTap: function (e) {
-        var curType = e.currentTarget.dataset.index;
         this.setData({
-            currentType: curType
+            currentType: e.currentTarget.dataset.index
         });
         this.setSearchInitData()
         this.getPayOrder()
@@ -45,15 +214,21 @@ Page({
         // 生命周期函数--监听页面加载
     },
     /**
-     * onShow 初始化页面搜索参数，并加载订单列表
+     * onShow
+     * 初始化页面搜索参数，并加载订单列表
+     * 如果正在获取二维码，那么除了上述操作，还会根据获取结果进行用户操作提示
+     * onShow触发时机：
+     * 1、支付完毕回到页面
+     * 2、查看订单详情后，回到页面
      */
     onShow: function () {
         this.setSearchInitData()
         if (this.data.isGettingQrcode) {
-            this.setData({
-                isGettingQrcode: false
+            hasQrcode((has_qr_code) => {
+                this.data.has_qrcode = has_qr_code
+                this.data.isGettingQrcode = false
+                this.getPayOrderAndShowGettingQrcode()
             })
-            this.getPayOrderAndshowGettingQrcode()
         } else {
             this.getPayOrder();
         }
@@ -71,38 +246,65 @@ Page({
             loadingMore: true
         })
     },
+    /**
+     * orderCancel
+     * 取消订单
+     * @param e
+     */
     orderCancel: function (e) {
         this.orderOps(e.currentTarget.dataset.id, "cancel", "确定取消订单？");
     },
-    getPayOrderAndshowGettingQrcode: function(){
+    /**
+     * getPayOrderAndShowGettingQrcode
+     * 除了获取订单列表外，根据二维码获取情况，给用户操作提示
+     */
+    getPayOrderAndShowGettingQrcode: function(){
         this.getPayOrder()
         wx.showLoading({
             title: '获取闪寻码中',
             mask: true
         })
-        setTimeout(()=>{
-            wx.hideLoading()
-            wx.showToast({
-                title: '获取成功',
-                icon: 'success',
-                mask: true,
-                duration: 800,
-                success: function () {
-                    setTimeout(function () {
-                        app.alert({
-                            title: '赠品提示',
-                            content: '已返还5毛到您的账户，用于5次免费的失物通知！',
-                            cb_confirm: () => {
-                                app.alert({
-                                    title: '查看提示',
-                                    content: '在入口页【我的】—【个人信息】查看您的专属闪寻码',
-                                })
-                            }
+        if (this.data.has_qrcode) {
+            //获取成功
+            setTimeout(() => {
+                wx.hideLoading()
+                wx.showToast({
+                    title: '获取成功',
+                    icon: 'success',
+                    mask: true,
+                    duration: 800,
+                    success: function () {
+                        setTimeout(function () {
+                            app.alert({
+                                title: '赠品提示',
+                                content: '已发放5次免费的失物通知！',
+                                cb_confirm: () => {
+                                    app.alert({
+                                        title: '查看提示',
+                                        content: '在入口页【我的】—【个人信息】查看您的专属闪寻码',
+                                    })
+                                }
+                            })
+                        }, 700)
+                    }
+                })
+            }, 500)
+        } else {
+            //获取失败
+            setTimeout(() => {
+                wx.hideLoading()
+                app.alert({
+                    title: "跳转提示",
+                    content: "联系技术支持帮您获取二维码",
+                    cb_confirm: () => {
+                        wx.redirectTo({
+                           url: '/pages/Mine/connect/index'
                         })
-                    }, 700)
-                }
-            })
-        }, 500)
+                    }
+                })
+            }, 500)
+        }
+
     },
     /**
      * getPayOrder 获取一页订单列(更多标记)
@@ -131,42 +333,59 @@ Page({
             }
         });
     },
+    /**
+     * 如果订单余额垫付，则增加一层扣除余额
+     * @param e
+     */
     toPay: function (e) {
-        var that = this;
-        wx.request({
-            url: app.buildUrl("/order/pay"),
-            header: app.getRequestHeader(),
-            method: 'POST',
-            data: {
-                order_sn: e.currentTarget.dataset.id
-            },
-            success: function (res) {
-                var resp = res.data;
-                if (resp.code != 200) {
-                    app.alert({ "content": resp.msg });
-                    return;
-                }
-                var pay_info = resp.data.pay_info;
-                wx.requestPayment({
-                    'timeStamp': pay_info.timeStamp,
-                    'nonceStr': pay_info.nonceStr,
-                    'package': pay_info.package,
-                    'signType': 'MD5',
-                    'paySign': pay_info.paySign,
-                    'success': function (res) {
-                        //支付成功
-                        if (res.errMsg == "requestPayment:ok" && !app.globalData.has_qrcode) {
-                            that.setData({
-                                isGettingQrcode: true
-                            })
-                            getQrcodeFromWechat()
-                        }
-                    },
-                    'fail': function (res) {
-                    }
-                });
-            }
-        });
+        let dataset = e.currentTarget.dataset
+        //订单流水号和垫付的余额数
+        let order_sn = dataset.id
+        let balance_discount = dataset.balance
+        //订单中购买的非周边产品
+        let sms_num = dataset.sms
+        let sms_pkg_num = dataset.sms_pkg
+        let qr_code_num = dataset.qr_code
+        if(balance_discount == 0) {
+            //无余额垫付
+            orderPay(order_sn, () => {
+                this.orderPaySuccessCallback(qr_code_num, sms_pkg_num, sms_num)
+            })
+        }else{
+            //余额垫付
+            orderPay(order_sn, () => {
+                changeUserBalance(-balance_discount, () => {
+                    this.orderPaySuccessCallback(qr_code_num, sms_pkg_num, sms_num)
+                })
+            })
+        }
+    },
+    /***
+     * 根据是否购买了非周边产品，支付后进行回调操作
+     * @param qr_code_num 购买的二维码数量
+     * @param sms_pkg_num 购买的套餐包数量
+     * @param sms_num 购买的按量计费消息数
+     */
+    orderPaySuccessCallback: function(qr_code_num=0, sms_pkg_num=0, sms_num=0){
+        if (qr_code_num) {
+            //操作member和qr_code表
+            this.data.isGettingQrcode = true
+            getQrcodeFromWechat(()=>{
+                changeMemberSmsTimes(app.globalData.buyQrCodeFreeSmsTimes, ()=>{})
+            })
+        }
+        else if(sms_pkg_num) {
+            //操作sms_pkg表
+            addSmsPkg(()=>{
+               smsBuySuccessCommonCallback('短信包购买成功')
+            })
+        }
+        else if(sms_num){
+            //操作qr_code表
+            changeMemberSmsTimes(sms_num, ()=>{
+                smsBuySuccessCommonCallback('短信购买成功')
+            })
+        }
     },
     orderConfirm: function (e) {
         this.orderOps(e.currentTarget.dataset.id, "confirm", "确定收到？");
@@ -174,7 +393,7 @@ Page({
     orderComment: function (e) {
         wx.navigateTo({
             url: "/mall/pages/my/comment?order_sn=" + e.currentTarget.dataset.id
-        });
+        })
     },
     orderOps: function (order_sn, act, msg) {
         var that = this;
--- frontend/pages/Mine/userinfo/index.js
+++ frontend/pages/Mine/userinfo/index.js
@@ -1,4 +1,91 @@
 const app = getApp();
+const util = require('../../../utils/util')
+const toOrderSpecialProduct = function (data={}) {
+  wx.showToast({
+    title: '前往下单',
+    icon: 'loading',
+    success: res => {
+      setTimeout(function () {
+        wx.navigateTo({
+          'url': '/mall/pages/order/index?data=' + JSON.stringify(data)
+        })
+      }, 200)
+    }
+  })
+}
+
+
+const balanceCharge = function (pay_price=0.01, cb_success=()=>{}) {
+  wx.request({
+    url: app.buildUrl('/balance/order'),
+    header: app.getRequestHeader(),
+    data: {
+      price: pay_price
+    },
+    method: 'POST',
+    success: res => {
+      let resp = res.data
+
+      //下单失败提示后返回
+      if (resp['code'] !== 200) {
+        app.alert({
+          content: resp['msg']
+        })
+        return
+      }
+
+      //下单成功调起支付
+      let pay_data = resp['data']
+      wx.requestPayment({
+        timeStamp: pay_data['timeStamp'],
+        nonceStr: pay_data['nonceStr'],
+        package: pay_data['package'],
+        signType: pay_data['signType'],
+        paySign: pay_data['paySign'],
+        success: res => {
+          //支付成功，继续发布
+          cb_success()
+        },
+        fail: res => {
+          app.alert({content: "支付失败"})
+        }
+      })
+    },
+    fail: res => {
+      app.serverBusy()
+    }
+  })
+}
+
+/**
+ * changeUserBalance
+ * 扣除(改变)用户余额
+ * @param unit 改变量
+ * @param cb_success 回调函数
+ */
+const changeUserBalance = function (unit = 0, cb_success = () => {}, cb_fail=()=>{}) {
+  wx.showLoading({
+    title: "扣除余额中"
+  })
+  wx.request({
+    url: app.buildUrl("/member/balance/change"),
+    header: app.getRequestHeader(),
+    data: {
+      unit: unit,
+      note: "余额充值"
+    },
+    success: res => {
+      cb_success()
+    },
+    fail: res=>{
+      cb_fail()
+    },
+    complete: res => {
+      wx.hideLoading()
+    }
+  })
+}
+
 
 Page({
   data: {
@@ -6,10 +93,20 @@ Page({
     has_qrcode: false,
     show_qrcode: false,
     qr_code: "",
-    hiddenNameModal: true,
-    hiddenMobileModal: true,
-    hiddenContactModal: true,
-    contact_img: app.globalData.static_file_domain + "/static/QRcode.jpg"
+    hiddenNameModal: true, //编辑姓名
+    hiddenMobileModal: true, //编辑手机
+    hiddenContactModal: true, //余额提现，客服
+    hiddenSmsTimesModal: true, //短信按量购买
+    hiddenSmsPkgModal: true, //短信套餐包购买
+    hiddenIntroduceModal: true, //二维码使用介绍
+    hiddenBalanceRecharge: true, //充值余额
+    hiddenSmsDetailModal: true, //短信余额详情
+    contact_img: app.globalData.static_file_domain + "/static/QRcode.jpg",
+    intro_url: app.globalData.static_file_domain+ "/static/introduce.mp4",
+    intro_init_time: 0, //
+    balance_recharge_amount: "",
+    sms_num: "",
+    sms_pkg_price: app.globalData.smsPkgProductPrice
   },
   onLoad: function () {
   },
@@ -38,14 +135,17 @@ Page({
           nickname: info['nickname'],
           name: info['name'],
           mobile: info['mobile'],
-          balance: info['balance'],
+          balance: util.toFixed(info['balance'],2),
           has_qrcode: info['has_qrcode'],
-          qr_code: info['qr_code']
+          qr_code: info['qr_code'],
+          member_notify_times: info['m_times'],
+          pkg_notify_times: info['p_times'],
+          total_notify_times: info['m_times'] + info['p_times'],
+          pkg_expire: info['p_expire']
         })
       },
       fail: (res) => {
-        app.serverBusy()
-        wx.navigateBack()
+        app.serverBusy(()=>{wx.navigateBack()})
       }
     })
   },
@@ -71,10 +171,10 @@ Page({
   },
   showToast: function (title = '', icon = 'none', duraton = 1000, mask = true) {
     wx.showToast({
-      'title': title,
-      'icon': icon,
-      'duraton': duraton,
-      'mask': mask
+      title: title,
+      icon: icon,
+      duraton: duraton,
+      mask: mask
     })
   },
   confirmNameEdit: function (e) {
@@ -121,13 +221,13 @@ Page({
    * toGetQrCode 判断没有姓名或手机号提示用户补上，否则继续下单获取二维码
    */
   toGetQrCode: function(){
-    var name = this.data.name
-    var mobile = this.data.mobile
-    if (name == "-") {
+    let name = this.data.name
+    let mobile = this.data.mobile
+    if (!name) {
       app.alert({ 'content': '姓名不能为空' })
       return
     }
-    if (mobile == "-") {
+    if (!mobile) {
       app.alert({ 'content': '手机不能为空' })
       return
     }
@@ -142,23 +242,16 @@ Page({
       type: 'toBuy',
       goods: [{'id': app.globalData.qrcodeProductId, 'price': app.globalData.qrcodePrice, 'number': 1}]
     }
-    wx.showToast({
-      title: '前往下单',
-      icon: 'loading',
-      success: res => {
-         setTimeout(function(){
-           wx.navigateTo({
-             'url' : '/mall/pages/order/index?data='+ JSON.stringify(data)
-           })
-         }, 200)
-      }
-    })
+    toOrderSpecialProduct(data)
   },
   checkQrCode: function () {
     this.setData({
       show_qrcode: !this.data.show_qrcode
     })
   },
+  /**
+   * onWithDrawTap 用户按下提现按钮后根据余额，做出操作提示
+   */
   onWithDrawTap: function () {
     if (this.data.balance < 10) {
       app.alert({
@@ -171,11 +264,19 @@ Page({
       hiddenContactModal: false
     })
   },
+  /**
+   *
+   * @param e
+   */
   cancelContact: function (e) {
     this.setData({
       hiddenContactModal: true
     })
   },
+  /**
+   * previewImage 预览并保存二维码
+   * @param e
+   */
   previewImage: function (e) {
     let image = e.currentTarget.dataset.src
     wx.previewImage({
@@ -183,14 +284,181 @@ Page({
       urls: [image] // 需要预览的图片http链接列表
     })
   },
+  /**
+   * toRechargeBalance 用户按下余额充值按钮显示输入充值金额的模态框
+   */
+  toRechargeBalance: function(){
+    this.setData({
+      hiddenBalanceRecharge: false
+    })
+  },
+  listenBalanceRecharge: function(e){
+    this.data.balance_recharge_amount = e.detail.value
+  },
+  /**
+   * confirmRechargeBalance 与用户核对充值收费金额
+   * 如果用户确认就继续进入收费充值过程
+   * 否则，终止操作
+   */
+  confirmRechargeBalance: function(){
+    if(!this.data.balance_recharge_amount){
+      this.cancelRechargeBalance()
+      return
+    }
+    let pay_price = util.toFixed(this.data.balance_recharge_amount*1.01, 2)
+    if(!pay_price) {
+      this.cancelRechargeBalance()
+      return
+    }
+
+    console.log(pay_price)
+    app.alert({
+      title: '充值确认',
+      content: '您需要支付'+pay_price+'元，确认充值？',
+      showCancel: true,
+      cb_confirm: ()=>{
+        this.doBalanceRecharge(pay_price)
+        this.cancelRechargeBalance() //隐藏输入金额的模态框
+      },
+      cb_cancel: this.cancelRechargeBalance() //隐藏金额输入的模态框
+    })
+  },
+  /**
+   * doBalanceRecharge 充值余额的实际操作函数
+   * 先收费，如果支付成功增加余额，
+   * 如果收费成功但是余额增加失败则提示用户联系技术人员操作增加
+   * @param pay_price
+   */
+  doBalanceRecharge: function (pay_price = 0.01) {
+    balanceCharge(pay_price, () => {
+      changeUserBalance(pay_price, () => {
+        wx.showToast({
+          title: '充值成功',
+          duration: 500
+        })
+        this.setData({
+          balance: this.data.balance + pay_price
+        })
+      }, () => {
+        app.alert({
+          title: '跳转提示',
+          content: '联系技术人员充值余额',
+          cb_confirm: () => {
+            wx.navigateTo({url: '/pages/Mine/connect/index'})
+          }
+        })
+      })
+    })
+  },
+  /**
+   * cancelRechargeBalance 隐藏金额输入的模态框
+   */
+  cancelRechargeBalance: function(){
+    this.setData({
+      hiddenBalanceRecharge: true,
+      balance_recharge_amount: ""
+    })
+  },
+  /**
+   * toRechargeSms 根据用户选择的按量购买或者短信包进入下一步的操作
+   */
+  toRechargeSms: function(){
+    wx.showActionSheet({
+      itemList: ['按量计费', '优惠短信包'],
+      success: (res) => {
+        if(res.tapIndex == 0){
+          //出现编辑购买条数
+          app.alert({
+            title: '计价提示',
+            content: '0.1元/1条。继续购买？',
+            showCancel: true,
+            cb_confirm: ()=>{
+              this.setData({
+                hiddenSmsTimesModal: false
+              })
+            }
+          })
+        } else {
+          //直接跳转下单
+          this.setData({
+            hiddenSmsPkgModal: false
+          })
+        }
+      }
+    })
+  },
+  listenSmsCnt: function(e){
+    this.data.sms_num = e.detail.value
+  },
+  /**
+   * 确定购买指定条数的短信
+   * @param e
+   */
+  confirmSmsTimes: function(e){
+    let data = {
+      type: 'toBuy',
+      goods: [{'id': app.globalData.smsProductId, 'price': app.globalData.smsProductPrice, 'number': parseInt(this.data.sms_num)}]
+    }
+    toOrderSpecialProduct(data)
+    this.setData({
+      hiddenSmsTimesModal: true
+    })
+  },
+  /**
+   * 取消购买指定条数的短信
+   */
+  cancelSmsTimes: function(){
+    this.setData({
+      hiddenSmsTimesModal: true,
+      sms_num: ""
+    })
+  },
+  confirmSmsPkg: function(){
+    let data = {
+      type: 'toBuy',
+      goods: [{'id': app.globalData.smsPkgProductId, 'price': app.globalData.smsPkgProductPrice, 'number': 1}]
+    }
+    toOrderSpecialProduct(data)
+    this.setData({
+      hiddenSmsPkgModal: true
+    })
+  },
+  cancelSmsPkg: function(){
+    this.setData({
+      hiddenSmsPkgModal: true
+    })
+  },
   toIntroduce: function () {
-
-
+    this.setData({
+      hiddenIntroduceModal: false
+    })
+  },
+  cancelIntroduce: function(){
+    //停止播放并隐藏模态框
+    this.setData({
+      hiddenIntroduceModal: true
+    })
   },
   toEditPhone: function () {
     wx.navigateTo({
       url: '/pages/Qrcode/Mobile/index'
     })
     this.cancelMobileEdit()
+  },
+  toLookupSms: function () {
+    // let pkg_times = this.data.pkg_notify_times
+    // let m_times = this.data.member_notify_times
+    // wx.showModal({
+    //   title: '次数明细',
+    //   content: (pkg_times?  pkg_times+'条至'+this.data.pkg_expire+'。' : '') + (m_times? '不限期'+m_times+'条。':'')
+    // })
+    this.setData({
+      hiddenSmsDetailModal: false
+    })
+  },
+  closeSmsDetail: function () {
+    this.setData({
+      hiddenSmsDetailModal: true
+    })
   }
 })
