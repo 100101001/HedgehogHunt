2fc70e9dcb31c6fb431d6a0a489a3e48e40fca80
100101001 <1552729@tongji.edu.cn>
2020-05-06 01:23:48 +0800

    200506后端部署运行脚本

---
+++ backend/run_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#启动flask
+python /code/manager.py runserver & 
+
+#启动worker
+#celery worker -A celery_tasks.main -l info -f /opt/hrms/logs/celery.log &   #这里注意日志位置要写绝对路径
+celery -A application.celery worker -Q log_queue,sync_queue,recommend_queue,subscribe_queue,sms_queue,mall_queue -l info &
+#启动beat
+celery -A application.celery beat -l info
