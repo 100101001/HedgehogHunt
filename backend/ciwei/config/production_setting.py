5d7425201e97dc63943a25f7e38466112d3d77be
100101001 <1552729@tongji.edu.cn>
2020-05-10 16:30:59 +0800

    200510后端体验修复
    支付的domain出错

--- backend/ciwei/config/production_setting.py
+++ backend/ciwei/config/production_setting.py
@@ -6,7 +6,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"
 
 APP = {
-    'domain':'https://ciwei.opencs.cn'
+    'domain':'https://xunhui.opencs.cn'
     #'domain':'https://all.opencs.cn.w.kunlungr.com'
 }
 
