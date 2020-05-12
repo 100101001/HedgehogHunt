c86f51b5d03b52b91824f6156eee7241bf5cfed3
100101001 <1552729@tongji.edu.cn>
2020-05-12 18:10:49 +0800

    部署200512
    1、带上测试数据库
    2、初始化DDL

--- backend/init/ciwei_db_deploy.sql
+++ backend/init/ciwei_db_deploy.sql
@@ -2,14 +2,8 @@ SET NAMES utf8mb4;
 
 CREATE DATABASE IF NOT EXISTS `ciwei_db_test`;
 USE `ciwei_db_test`;
-SELECT @table_count:=count(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA='ciwei_db_test';
-IF @table_count < 1 THEN
-  source ddl/create_tables.sql
-END IF
+source /docker-entrypoint-initdb.d/ddl/create_tables.sql;
 
 CREATE DATABASE IF NOT EXISTS `ciwei_db`;
 USE `ciwei_db`;
-SELECT @table_count:=count(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA='ciwei_db';
-IF @table_count < 1 THEN
-  source ddl/create_tables.sql
-END IF
+source /docker-entrypoint-initdb.d/ddl/create_tables.sql;
