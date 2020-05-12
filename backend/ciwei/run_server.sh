00c80d01c5436c607b1ffd95d9702cb04ec28838
100101001 <1552729@tongji.edu.cn>
2020-05-12 19:08:37 +0800

    部署200512

--- backend/ciwei/run_server.sh
+++ backend/ciwei/run_server.sh
@@ -2,8 +2,9 @@
 
 #启动flask
 # python /code/manager.py runserver &
-uwsgi --ini /code/uwsgi_docker.ini &
+uwsgi --ini /code/uwsgi_docker.ini
+sleep 10
 #启动worker
-celery -A application.celery worker -Q log_queue,sync_queue,recommend_queue,subscribe_queue,sms_queue,mall_queue -l info &
+celery -A application.celery worker -Q log_queue,sync_queue,recommend_queue,subscribe_queue,sms_queue,mall_queue -l info
 #启动beat
 celery -A application.celery beat -l info
