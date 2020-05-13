3d105dc1b7e86850508ed4ec2fb9a74b0d860b63
100101001 <1552729@tongji.edu.cn>
2020-05-14 01:28:18 +0800

    后端测试0514价格

--- backend/ciwei/config/test_setting_docker.py
+++ backend/ciwei/config/test_setting_docker.py
@@ -25,3 +25,46 @@ REDIS = {
     },
     'CACHE_REDIS_PASSWORD': 'lyx147'
 }
+
+CONSTANTS = {
+    'time_format_short': '%Y-%m-%d %H:%M',
+    'time_format_long': '%Y-%m-%d %H:%M:%S',
+    'sub_time_format': '%Y年%m月%d日 %H:%M',
+    'sys_author': {
+        'member_id': '100001',
+        'openid': 'bPk3u33u+sqUiuxJ/+ubfQ==',
+        'avatar': 'https://wx.qlogo.cn/mmopen/vi_32/7jCR4QflwchksTBcyakicSepWVQdfbHIQL2glRrkY7ic52iaXqfuBb2tlQ8ELlaGWZDXFgKM4zAMeQZSiaaJtibI3gQ/132',
+        'nickname': '鲟回-管理员'
+    },
+    'default_lost_loc': '不知道###不知道###0###0',
+    'default_loc': ['不知道', '不知道', 0, 0],
+    'is_all_user_val': '1',
+    'stuff_type': {
+        'goods': 1,
+        'thanks': 0
+    },
+    'page_size': 10,
+    'max_pages_allowed': 50,
+    'sp_product': {
+        'client_mobile': '17717852647',
+        'qrcode': {
+            'id': 15,
+            'price': 0.02
+        },
+        'sms_pkg': {
+            'id': 16,
+            'price': 4.5
+        },
+        'sms': {
+            'id': 17,
+            'price': 0.1
+        },
+        'top': {
+            'price': 0.02,
+            'days': 7
+        },
+        'free_sms': {
+            'times': 5
+        }
+    }
+}
