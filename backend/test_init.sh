78a64d88b39e03a22defbdf19777b4ce18dc72db
100101001 <1552729@tongji.edu.cn>
2020-05-15 00:19:10 +0800

    测试初始化脚本

--- backend/test_init.sh
+++ backend/test_init.sh
@@ -1,4 +1,4 @@
-cp ./init/ddl/test_db_clean.sql ./redis/data/
+cp ./init/ddl/test_redis_clean.txt ./redis/data/
 docker exec backend_db bash -c "mysql -uroot -pwcx9517530 < /docker-entrypoint-initdb.d/ddl/test_db_clean.sql"
 docker exec backend_redis bash -c "cat /data/test_redis_clean.txt | redis-cli -a lyx147"
 curl -XPOST localhost:9200/goods_test/_delete_by_query -H "Content-Type:application/json" -d '{"query":{"match_all":{}}}'
