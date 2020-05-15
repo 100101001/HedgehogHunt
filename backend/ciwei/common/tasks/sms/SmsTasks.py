c65153b7ddeea19c77197fa3c9a9bc6805752ce1
100101001 <1552729@tongji.edu.cn>
2020-05-15 23:45:18 +0800

    二维码通知不收费

--- backend/ciwei/common/tasks/sms/SmsTasks.py
+++ backend/ciwei/common/tasks/sms/SmsTasks.py
@@ -32,10 +32,14 @@ def notifyQrcodeOwner(params=None):
     rcv_openid = trig_rcv.get('rcv_openid')
     handler = SmsNotifyHandler(openid=rcv_openid)
     # 判断是否发送短信
-    if not handler.validUser() or handler.tooFreq() or not handler.hasCharger():
-        return
-    send_ok = handler.sendSmsNotify(goods_name=goods_name, location=put_location, trig_rcv=trig_rcv)
-    if send_ok:
-        handler.charge()
-    db.session.commit()
+    # if not handler.validUser() or handler.tooFreq() or not handler.hasCharger():
+    #     return
+    if handler.validUser() and not handler.tooFreq():
+        handler.sendSmsNotify(goods_name=goods_name, location=put_location, trig_rcv=trig_rcv)
+        db.session.commit()
+    # send_ok = handler.sendSmsNotify(goods_name=goods_name, location=put_location, trig_rcv=trig_rcv)
+    # if send_ok:
+    #     handler.charge()
+    # db.session.commit()
+
 
